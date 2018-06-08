6ea65ae1bdd7f8e6ee1e030f93206c15dd7c0821
Serhii <shulhin@asta.mobi>
2018-06-08 15:04:35 +0300

    .

---
+++ list.js
@@ -0,0 +1,50 @@
+var data = {
+  "video": [{
+      "txt": "Coursehunters",
+      "src": "https://coursehunters.net/course/stroim-node-js-rest-api-s-loopback"
+    },
+    {
+      "txt": "Egghead",
+      "src": "https://egghead.io/courses/build-a-node-js-rest-api-with-loopback"
+    },
+    {
+      "txt": "Get started",
+      "src": "https://www.youtube.com/watch?v=Llr18lNuYIo"
+    },
+    {
+      "txt": "Шукайте ще...",
+      "src": "https://www.google.com.ua"
+    }
+  ],
+  "docs": [{
+      "txt": "Freecodecamp",
+      "src": "https://medium.freecodecamp.org/build-restful-api-with-authentication-under-5-minutes-using-loopback-by-expressjs-no-programming-31231b8472ca"
+    },
+    {
+      "txt": "Шукайте ще...",
+      "src": "https://www.google.com.ua"
+    }
+  ],
+  "basic": [{
+      "txt": "Основи (повна дока)",
+      "src": "http://loopback.io/doc/en/lb3/"
+    },
+    {
+      "txt": "Використання Монго БД",
+      "src": "http://loopback.io/doc/en/lb3/MongoDB-connector.html"
+    }
+  ],
+  "tasks": [{
+      "txt": "Task 1",
+      "task": "Обовʼязково пройти.",
+      "desc": "Познайомитися з фреймворком.",
+      "src": "http://loopback.io/getting-started/"
+    },
+    {
+      "txt": "Task 2",
+      "task": "Обовʼязково пройти.",
+      "desc": "Створити на свій розсуд апп, хоча б, з 3-ма модельками. Залити на гітхаб, відправити посилання мені 'Телеграм'.",
+      "src": "http://loopback.io/getting-started/"
+    }
+  ]
+}
